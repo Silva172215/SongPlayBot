8d5f73213a255d624dd61256a749979b8ba0f69b
Silva172215 <99672125+Silva172215@users.noreply.github.com>
2022-02-14 12:14:06 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-API_ID = int(os.getenv("16460251"))
-API_HASH = os.getenv("67068034caedfb02e3bbfef339f827bd")
-BOT_TOKEN = os.getenv("5121380728:AAEOWGflH_3BR4VMUHfjdgH7BF_yjJLDlLs")
-DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
